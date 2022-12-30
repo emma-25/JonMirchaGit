a994369f2b00d9b0ed952b6bfe8c849746f76ce4
emma-25 <mombellitic@gmail.com>
2022-12-30 02:06:15 -0300

    resolviendo conflicto entre ramas

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 console.log('Aprendiendo Git, GitHub');
 console.log('Aprendiendo a utilizar git y realizando merge');
 console.log('GitLab');
+
+let sum;
+function suma(a, b) {
+    return 'La suma es: sum';
+}
+
+console.log(suma(10, 7));
