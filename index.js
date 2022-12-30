c8c28dd679b70d999771ff04a22b9baf4bed5367
emma-25 <mombellitic@gmail.com>
2022-12-30 02:15:06 -0300

    isuuuu

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ console.log('Aprendiendo Git, GitHub');
 console.log('Aprendiendo a utilizar git y realizando merge');
 console.log('GitLab');
 
+console.log('Funcion Suma');
+
 function suma(a, b) {
     return 'La suma es: sum';
 }
